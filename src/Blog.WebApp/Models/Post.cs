a8cf5ce92c6f7aa0b48b9f4429240da5cb99f9b6
José Campelo <josecampelojr@gmail.com>
2025-10-15 06:58:14 -0300

    feat: Cria a entidade de domínio Post
    - Adiciona a classe `Post` na pasta `Models` para representar a entidade central do blog.
    - Define as propriedades essenciais de um post, como Título, Conteúdo e Data de Publicação, utilizando Data Annotations para validação (`Required`, `StringLength`).
    - Estabelece o relacionamento entre um `Post` e seu autor através da chave estrangeira `ApplicationUserId` e da propriedade de navegação `ApplicationUser`.

---
+++ src/Blog.WebApp/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using Blog.WebApp.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.WebApp.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Titulo { get; set; }
+
+    [Required]
+    public string Conteudo { get; set; }
+
+    public DateTime DataPublicacao { get; set; }
+
+    public string ApplicationUserId { get; set; }
+
+    public virtual ApplicationUser? ApplicationUser { get; set; }
+}
